aa75ba3487bb3a65fe8af18bdea36edd0382f516
Vamshi Krishna <vamshikrishnakommu0908@gmail.com>
2026-05-05 09:09:11 +0530

    updating backend structure

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+
+const { configDotenv } = require("dotenv");
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
